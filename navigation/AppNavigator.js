f2e77250a6dcebb117f715c403a5b363d874faf9
saacevedo <santur1989@gmail.com>
2025-08-01 17:05:13 -0500

    Update AppNavigator.js

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import TaskListScreen from '../screens/TaskListScreen';
-import AddTaskScreen from '../screens/AddTaskScreen';
+import ListScreen from '../screens/ListScreen';
 import AddContactScreen from '../screens/AddContactScreen';
 
 const Stack = createStackNavigator();
@@ -11,8 +10,7 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Tasks" component={TaskListScreen} />
-        <Stack.Screen name="AddTask" component={AddTaskScreen} />
+        <Stack.Screen name="Directorioo123" component={ListScreen} />
         <Stack.Screen name="AddContact" component={AddContactScreen} />
       </Stack.Navigator>
     </NavigationContainer>
