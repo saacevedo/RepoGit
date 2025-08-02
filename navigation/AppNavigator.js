93a468da62f3fff0a6bac71ad2905fa5c9d29e35
saacevedo <santur1989@gmail.com>
2025-08-01 21:10:45 -0500

    Borrado de ListScreenEnsayo

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import ListScreen from '../screens/ListScreen';
+import ListScreen from '../screens/ListScreenn';
 import AddContactScreen from '../screens/AddContactScreen';
 
 const Stack = createStackNavigator();
